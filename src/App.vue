2eda5061f3d2f998b244eb937fe840c8a1d7cd6d
Mr.Black <westin007@gmail.com>
2021-02-24 13:01:01 -0600

    prove to me this is working - Mr. Black

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="The War For Rayuba Map Placeholder"/>
   </div>
 </template>
 

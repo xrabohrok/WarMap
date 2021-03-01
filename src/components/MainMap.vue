40bc0b0f28feca47c79113216d58582dfd4b909a
Mr.Black <westin007@gmail.com>
2021-03-01 17:23:04 -0600

    that nice overlapped grid look - Mr.Black

--- src/components/MainMap.vue
+++ src/components/MainMap.vue
@@ -1,7 +1,6 @@
 <template>
-    <div id="mainmap">
-        You should see something here no matter what
-        <div class="row" v-for="row in mapSource" v-bind:key="row.id">
+    <div id="mainMap">
+        <div class="row" v-for="row in mapSource" v-bind:key="row.id" :style="rowPosition(row.id, mapSource.length)">
             {{row.id}} : 
             <MapTile v-for="item in row.set" :key="item" :title="item"/>
         </div> 
@@ -22,14 +21,38 @@ export default {
     components:{
         MapTile
     },
+    methods: {
+        rowPosition: function(rowId, maxRow){
+            var horizontalPos = Math.floor((rowId-1) / (maxRow + 1) * 100);
+            // console.log(`${rowId} : against ${maxRow}, ${horizontalPos}`)
+            return {
+                "top": `${horizontalPos}%`
+            };
+        }
+    },
     name: 'MainMap'
 }
 </script>
 
 <style scoped>
+#mainMap{
+    position: relative;
+    height: 12.5em;
+    width: 80%;
+    margin-left: 10%;
+    margin-right: 10%;
+}
+
 .row {
   display: flex;
   justify-content: center;
+  padding: 0;
+  position: absolute;
+  left:0;
+  right:0;
+  margin: auto;
+  width: 100%;
+  height: auto;
 }
 
 </style>
--- src/components/MapTile.vue
+++ src/components/MapTile.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mapTile">
+    <div class="mapTile" :style="tileWidth">
         <img src="../assets/mapTiles/map-tile.png">
         <div class="label">
             {{title}}
@@ -11,6 +11,14 @@
 
 export default {
     name: 'MapTile',
+    computed:{
+        tileWidth: function(){
+            return{
+                "width": `${(100/14)}%`
+            }
+        }
+
+    },
     props: {
         title: String
     }
@@ -20,16 +28,25 @@ export default {
 
 <style scoped>
 
-div.maptile{
+div.mapTile{
     color: black;
-    padding: .3em;
+    padding: 0;
     text-align: center;
+    position: relative;
+    /* height: 3em; */
+}
+
+img{
+    width: 100%;
+    height: auto;
 }
 
 div.label{
     position: absolute;
-    top: 50%;
+    top: 20%;
     left: 50%;
+    transform: translate(-50%, -20%);
+    font-size: .9em;
 
 }
 

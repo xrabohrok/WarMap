3d834d1dc88559ac0d50b0ec1106ddc708bb2150
Mr.Black <westin007@gmail.com>
2021-04-20 20:40:13 -0500

    Added Grand Battle r3 - Mr.Black

--- src/assets/data/rounds/allRounds.js
+++ src/assets/data/rounds/allRounds.js
@@ -1,8 +1,9 @@
 import round0 from './round-0.json'
 import round1 from './round-1.json'
 import round2 from './round-2.json'
+import round3 from './round-3.json'
 
 
-const allRounds = [round0, round1, round2]
+const allRounds = [round0, round1, round2, round3]
 
 export default allRounds;

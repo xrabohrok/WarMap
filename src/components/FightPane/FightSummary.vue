781149d00b90c2ad3f76ffd8b15e969894fb814a
Samuel Armstrong <samuelhaydnarmstrong@outlook.com>
2021-06-02 21:37:07 +0100

    Style changes to the FightSummary area

--- src/components/FightPane/FightSummary.vue
+++ src/components/FightPane/FightSummary.vue
@@ -1,162 +1,245 @@
 <template>
     <div class="fightSummary">
-        <div class="fight" v-if="renderFight">
-            <div class="row"><h2>{{fightTitle}}</h2></div>
-            <div class="row header">
-                <div class="cell">Attacker</div>
-                <div class="cell">Defender</div>
-            </div>
+        <div
+            class="fight"
+            v-if="renderFight"
+            :class="{
+                bastionGradient: bastionAttacking,
+                pyreGradient: !bastionAttacking
+            }"
+        >
             <div class="row">
-                <div class="title cell" :class="{bastion: bastionAttacking, pyre: !bastionAttacking}">
-                    {{bastionAttacking ? "bastion" : "pyre"}}
+                <h2 class="fightTitle">{{ fightTitle }}</h2>
+            </div>
+
+            <div class="positionCards">
+                <div
+                    class="positionCard"
+                    :class="{
+                        victory: this.wasRevealed && attackerWin,
+                        defeat: this.wasRevealed && !attackerWin
+                    }"
+                >
+                    <img
+                        src="../../assets/pics/sword.png"
+                        class="attackIcon"
+                        draggable="false"
+                    />
+                    <h3 class="position">Attacker</h3>
+                    <h2 class="side">
+                        {{ bastionAttacking ? 'bastion' : 'pyre' }}
+                    </h2>
+
+                    <div v-show="isDuel">
+                        <StrikeLink
+                            class="cell"
+                            :fighterId="
+                                this.zoneFight.fighters[
+                                    bastionAttacking ? 'bastion' : 'pyre'
+                                ][0]
+                            "
+                            :round="round"
+                            :inputURL="getComicLink(true)"
+                        />
+                    </div>
                 </div>
-                <div class="title cell" :class="{bastion: !bastionAttacking, pyre: bastionAttacking}">
-                    {{bastionAttacking ?  "pyre" : "bastion"}}
+                <h3 class="versus">vs</h3>
+                <div
+                    class="positionCard"
+                    :class="{
+                        victory: this.wasRevealed && !attackerWin,
+                        defeat: this.wasRevealed && attackerWin
+                    }"
+                >
+                    <img
+                        src="../../assets/pics/shield.png"
+                        class="defendIcon"
+                        draggable="false"
+                    />
+                    <h3 class="position">Defender</h3>
+                    <h2 class="side">
+                        {{ bastionAttacking ? 'pyre' : 'bastion' }}
+                    </h2>
+
+                    <div v-show="isDuel">
+                        <StrikeLink
+                            class="cell"
+                            :fighterId="
+                                this.zoneFight.fighters[
+                                    bastionAttacking ? 'pyre' : 'bastion'
+                                ][0]
+                            "
+                            :round="round"
+                            :inputURL="getComicLink(false)"
+                        />
+                    </div>
                 </div>
             </div>
-            <div class="row duelLinks" v-show="isDuel" >
-                <StrikeLink class="cell" :fighterId="this.zoneFight.fighters[bastionAttacking ? 'bastion' : 'pyre'][0]" :round="round" :inputURL="getComicLink(true)"/>
-                <StrikeLink class="cell" :fighterId="this.zoneFight.fighters[bastionAttacking ? 'pyre' : 'bastion'][0]" :round="round" :inputURL="getComicLink(false)"/>
-            </div>
+
             <div class="row">
-                <Spoiler :hiddenText="fightOutcome"/>
+                <Spoiler :fightOutcome="fightOutcome" />
+                <div class="zebra" />
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
+import {linkLabel, cubariLink, showCubari} from '../../common/links';
+import Spoiler from './Spoiler.vue';
+import StrikeLink from '../elements/StrikeLink.vue';
+import {
+    SELECTING_GETTER,
+    CURRENT_ZONE_FIGHT,
+    CUR_FIGHTER_LINK,
+    CURRENT_ROUND,
+    CURRENT_ZONE_NAME,
+    CUR_SPOILER_REVEALED
+} from '../../state/getters';
 
-import { mapGetters } from 'vuex'
-import {linkLabel, cubariLink, showCubari} from '../../common/links'
-import Spoiler from './Spoiler.vue'
-import StrikeLink from '../elements/StrikeLink.vue'
-import { SELECTING_GETTER, CURRENT_ZONE_FIGHT, CUR_FIGHTER_LINK, CURRENT_ROUND, CURRENT_ZONE_NAME} from '../../state/getters'
-
-import {FIGHT} from '../DetailPaneDesktop'
+import {FIGHT} from '../DetailPaneDesktop';
 
 export default {
-    props:{
-        mode:Number
+    props: {
+        mode: Number
     },
-    components:{
+    components: {
         Spoiler,
         StrikeLink
     },
-    computed:{
-        renderFight: function(){
-            return this.mode === FIGHT && this.isSelecting
+    computed: {
+        renderFight: function () {
+            return this.mode === FIGHT && this.isSelecting;
         },
-        bastionAttacking: function(){
-            if(!this.selected == "NA") return -1
-            return this.zoneFight.attacker === "bastion"
+        bastionAttacking: function () {
+            if (!this.selected == 'NA') return -1;
+            return this.zoneFight.attacker === 'bastion';
         },
-        isSelecting: function(){
-            return this.selected !== "NA"
-        },
-        isDuel: function(){
-            return this.zoneFight.contest
-        },
-        zoneName: function(){
-            return this.currZone(this.selected)
-        },
-        fightTitle: function(){
-            var fightType = ""
-            if(this.zoneFight.contest && this.zoneFight.grandBattle){
-                return `Duel at ${this.zoneName}, but the Grand Battle at ${this.zoneFight.gbZone} reversed the outcome`
+        attackerWin: function () {
+            if (
+                (this.bastionAttacking &&
+                    this.zoneFight.outcome.bastion === 'win') ||
+                (!this.bastionAttacking &&
+                    this.zoneFight.outcome.pyre === 'win')
+            ) {
+                return true;
+            } else {
+                return false;
             }
-            else if(this.zoneFight.contest){
-                fightType = "Duel at "
+        },
+        isSelecting: function () {
+            return this.selected !== 'NA';
+        },
+        isDuel: function () {
+            return this.zoneFight.contest;
+        },
+        zoneName: function () {
+            return this.currZone(this.selected);
+        },
+        fightTitle: function () {
+            var fightType = '';
+            if (this.zoneFight.contest && this.zoneFight.grandBattle) {
+                return `Duel at ${this.zoneName}, but the Grand Battle at ${this.zoneFight.gbZone} reversed the outcome`;
+            } else if (this.zoneFight.contest) {
+                fightType = 'Duel at ';
+            } else if (this.zoneFight.grandBattle) {
+                return `Grand Battle at ${this.zoneFight.gbZone} took this area at ${this.zoneName}`;
+            } else if (this.zoneFight.clash) {
+                fightType = 'Clash at';
             }
-            else if(this.zoneFight.grandBattle){
-                return `Grand Battle at ${this.zoneFight.gbZone} took this area at ${this.zoneName}`
-            }
-            else if(this.zoneFight.clash){
-                fightType = "Clash at"
-            }
-            var name = this.zoneName
+            var name = this.zoneName;
 
-            return `${fightType} ${name}`
+            return `${fightType} ${name}`;
         },
-        fightOutcome: function(){
-            if(!(this.zoneFight.contest || this.zoneFight.grandBattle)) return "Not Available"
-            return `Bastion: ${this.zoneFight.outcome.bastion} , Pyre: ${this.zoneFight.outcome.pyre} \r\n ${'note' in this.zoneFight ? this.zoneFight.note : ''}`
+        fightOutcome: function () {
+            if (!(this.zoneFight.contest || this.zoneFight.grandBattle))
+                return {message: 'Not Available'};
+
+            const message =
+                this.zoneFight.outcome.bastion === 'win'
+                    ? 'Bastion Victory'
+                    : 'Pyre Victory';
+
+            return {
+                message: message,
+                note: this.zoneFight.note || ''
+            };
         },
         ...mapGetters({
-            currZone: CURRENT_ZONE_NAME,    
+            currZone: CURRENT_ZONE_NAME,
             selected: SELECTING_GETTER,
             zoneFight: CURRENT_ZONE_FIGHT,
             comicLink: CUR_FIGHTER_LINK,
             round: CURRENT_ROUND,
+            wasRevealed: CUR_SPOILER_REVEALED
         })
     },
-    methods:{
-   
-        getComicLink: function(attacker){
-            var facs = (this.bastionAttacking && attacker) || (!this.bastionAttacking && !attacker) ? "bastion" : "pyre"
-            return this.comicLink(facs)
+    methods: {
+        getComicLink: function (attacker) {
+            var facs =
+                (this.bastionAttacking && attacker) ||
+                (!this.bastionAttacking && !attacker)
+                    ? 'bastion'
+                    : 'pyre';
+            return this.comicLink(facs);
         },
-        linkLabel:linkLabel,
+        linkLabel: linkLabel,
         showCubari: showCubari,
-        cubariLink: cubariLink,
-
+        cubariLink: cubariLink
     },
-    name:"FightSummary"
-}
+    name: 'FightSummary'
+};
 </script>
 
 <style scoped>
-
-.fight{
-    margin-top: .4em;
+.fight {
     margin-left: auto;
     margin-right: auto;
-    order: 2;
     min-width: 20vw;
-    width: 80%;
-    height: 20vh;
     font-size: 1.1em;
-    overflow-y: auto;
-    right: 0%;
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
+    height: 25vh;
+    padding: 0em 4em;
+    margin-top: -10px;
+    margin-right: -10px;
 }
-.bastion{
-    color: rgb(69, 123, 221);
-    font-weight: bold;
+.bastionGradient {
+    background: linear-gradient(118deg, #5174a2 50%, #c07e85 50%);
+}
+.pyreGradient {
+    background: linear-gradient(118deg, #c07e85 50%, #5174a2 50%);
 }
 
-.duelLinks{
+.duelLinks {
     font-size: 1.4vw;
 }
 
-.pyre{
+.pyre {
     color: rgb(226, 119, 76);
     font-weight: bold;
 }
 
-.blockText{
+.blockText {
     font-size: 1.8em;
-    margin-bottom: .9em;
+    margin-bottom: 0.9em;
 }
 
-.cell{
+.cell {
     justify-content: center;
     width: 100%;
-    flex: 1
+    flex: 1;
 }
 
-.header{
+.header {
     font-weight: bold;
 }
 
-.linkset_checkbox{
-    margin-left: .9em;
+.linkset_checkbox {
+    margin-left: 0.9em;
     width: 1.5em;
 }
 
-.linkset{
+.linkset {
     display: flex;
     flex-direction: row;
     flex-flow: nowrap;
@@ -164,30 +247,108 @@ export default {
     margin-top: 1.2em;
 }
 
-.iconLink{
+.iconLink {
     width: 2em;
     position: relative;
     overflow: overlay;
     top: -50%;
     transform: translateY(40%);
-    margin-left: .4em;
-    fill:chartreuse;
+    margin-left: 0.4em;
+    fill: chartreuse;
 }
 
-.row{
+.row {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
-    align-items:flex-start;
-    margin-top: .3em
+    align-items: flex-start;
+    margin-top: 0.3em;
 }
 
-.combatant{
-    width:20%;
+.combatant {
+    width: 20%;
     min-width: 37vh;
-    margin-bottom: .7em;
-
+    margin-bottom: 0.7em;
 }
 
+.positionCards {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-bottom: 2em;
+}
+
+.positionCard {
+    background: #d3d3d363;
+    padding: 0.3em 3em 0.6em;
+    box-shadow: black 7px 7px 0px 0px;
+    position: relative;
+    width: 25%;
+    overflow: hidden;
+    transition: 0.6s;
+    min-height: 6.7em;
+}
+
+.victory {
+    background: #31ac61;
+}
+.defeat {
+    background: #d75054;
+}
+.position {
+    color: black;
+    font-size: 1.3em;
+    font-weight: bold;
+    opacity: 0.1;
+    text-transform: uppercase;
+    margin-left: -5em;
+    margin-top: 0.2em;
+    margin-bottom: 0.4em;
+}
+.side {
+    color: white;
+    text-transform: uppercase;
+    letter-spacing: 0.06em;
+    margin-bottom: 0.4em;
+    margin-top: 0em;
+}
+.attackIcon {
+    position: absolute;
+    width: 100px;
+    left: 140px;
+    top: 20px;
+    filter: opacity(0.1);
+}
+.defendIcon {
+    position: absolute;
+    width: 100px;
+    top: 18px;
+    left: 145px;
+    filter: opacity(0.1);
+}
+.fightTitle {
+    color: white;
+    height: 2em;
+}
+.versus {
+    color: white;
+    font-weight: bold;
+    font-size: 4em;
+    text-shadow: 6px 6px black;
+    padding: 0 0.5em;
+    margin: 0;
+}
+.zebra {
+    background-image: repeating-linear-gradient(
+        -45deg,
+        transparent,
+        transparent 20px,
+        black 20px,
+        black 40px
+    );
+    height: 1em;
+    width: 94%;
+    margin-top: -20px;
+}
 </style>
--- src/components/FightPane/Spoiler.vue
+++ src/components/FightPane/Spoiler.vue
@@ -1,52 +1,62 @@
 <template>
     <div class="spoiler" @click="whenClicked">
-        <div class="spoilerTitle" v-if="!wasRevealed">Click To Reveal Results</div>
-        <div class="spoilerText" v-show="wasRevealed">{{this.hiddenText}} </div>
+        <h2 class="spoilerTitle" v-if="!wasRevealed">Show Results</h2>
+        <div v-show="wasRevealed">
+            <h2 class="spoilerText">{{ this.fightOutcome.message }}</h2>
+            <p class="spoilerSubtext" v-show="this.fightOutcome.note">
+                {{ this.fightOutcome.note }}
+            </p>
+        </div>
     </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import {CUR_SPOILER_REVEALED} from '../../state/getters.js'
-import {REVEAL_SPOILER} from '../../state/mutations.js'
+import {mapGetters} from 'vuex';
+import {CUR_SPOILER_REVEALED} from '../../state/getters.js';
+import {REVEAL_SPOILER} from '../../state/mutations.js';
 
 export default {
-    name:"Spoiler",
-    props:{
-        hiddenText:String
+    name: 'Spoiler',
+    props: {
+        fightOutcome: Object
     },
-    data(){
+    data() {
         return {
             // isRevealed: false
-        } 
+        };
     },
-    methods:{
-        whenClicked(){
-            this.$store.commit(REVEAL_SPOILER)
+    methods: {
+        whenClicked() {
+            this.$store.commit(REVEAL_SPOILER);
         }
     },
-    computed:{
+    computed: {
         ...mapGetters({
             wasRevealed: CUR_SPOILER_REVEALED
         })
     }
-}
+};
 </script>
 
 <style scoped>
-.spoiler{
-    margin-left: auto;
-    margin-right:auto;
-    margin-top: .6em;
-    color: white;
-    margin-bottom: .7em;
-    background-color: rgb(119, 9, 70);
+.spoiler {
+    padding: 0em 0.8em 0.6em 0.8em;
+    color: black;
+    background-color: #efefef;
     width: 90%;
-    padding-top: .4em;
-    padding-bottom: .5em;
     white-space: pre;
-    
-
+    border: 4px solid black;
 }
-
+.spoilerTitle {
+    cursor: pointer;
+    margin: 0;
+    padding: 1em;
+}
+.spoilerText {
+    margin: 0;
+    padding: 1em;
+}
+.spoilerSubtext {
+    margin-top: 0;
+}
 </style>

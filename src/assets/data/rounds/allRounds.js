1f1222616dbfa3dcd74dceb8fb0176ac75b893f1
Mr.Black <westin007@gmail.com>
2021-07-29 22:37:43 -0500

    Hooked up battles - Mr.Black

--- src/assets/data/rounds/allRounds.js
+++ src/assets/data/rounds/allRounds.js
@@ -4,7 +4,8 @@ import round2 from "./round-2.json"
 import round3 from "./round-3.json"
 import round4 from "./round-4.json"
 import round5 from "./round-5.json"
+import round6 from "./round-6.json"
 
-const allRounds = [round0, round1, round2, round3, round4, round5]
+const allRounds = [round0, round1, round2, round3, round4, round5, round6]
 
 export default allRounds

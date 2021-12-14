20636b670defb3ab34b05dec5531565a6518002a
Mr.Black <westin007@gmail.com>
2021-12-13 22:38:29 -0600

    Creating tower cell - Mr. Black

---
+++ src/components/TowerTile.vue
@@ -0,0 +1,387 @@
+<template>
+  <div
+    class="mapTile"
+    :style="tileWidth"
+    @mouseenter="mousedOver()"
+    @mouseleave="mousedOut()"
+    @mouseout="mousedOut()"
+    v-touch="selectingClick"
+  >
+    <div class="zone_title" v-if="shouldShowZone">
+      {{ zoneTitle }}
+    </div>
+    <div class="terrainGroup" :class="{ terrainGroupHovered: isHovered }">
+      <transition name="stab">
+        <div class="selector" v-if="isSelected">
+          <img src="../assets/pics/chooser-knife.png" draggable="false" />
+          <div class="tileLabel">{{ title }}</div>
+        </div>
+      </transition>
+      <transition name="fall">
+        <img
+          :src="mapTilePath"
+          v-if="graphicsFilter"
+          v-bind:class="{ pyreOwned: pyreOwned, unOwned: unowned }"
+          class="terrainTile"
+          :style="fallTimeStyle"
+          draggable="false"
+        />
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+import { NEW_SELECTED } from "../state/mutations"
+import {
+  SELECTING_GETTER,
+  CURRENT_ZONE_GRANDBATTLE,
+  CUR_ZONE_ID,
+  CURRENT_ZONE_NAME,
+  ROUND_GRANDBATTLES,
+  TILE_IS_MINICLASH
+} from "../state/getters"
+
+export default {
+  name: "TowerTile",
+  data() {
+    return {
+      publicPath: process.env.BASE_URL,
+      hovering: false
+    }
+  },
+  computed: {
+    isHovered: function() {
+      return this.hovering
+    },
+    mapTilePath: function() {
+      // return `${this.title}.png`
+      return `tileimages/${this.title}.png`
+    },
+    isSelected: function() {
+      return this.selected == this.title
+    },
+    fallTimeStyle: function() {
+      return {
+        "--fall-time": `${(Math.random() + 0.1) * 1.1}s`
+      }
+    },
+
+    tileContested: function() {
+      return this.isContested(this.title) && !this.isGrandBattle(this.title)
+    },
+    tileGrandBattle: function() {
+      return this.isGrandBattle(this.title)
+    },
+    zoneTitle: function() {
+      return this.currZoneTitle(this.title)
+    },
+    shouldShowZone: function() {
+      return this.showZoneTitle(this.title) && this.zoneFilter
+    },
+    ...mapGetters({
+      tileOwner: TILE_OWNER,
+      selected: SELECTING_GETTER,
+      isGrandBattle: CURRENT_ZONE_GRANDBATTLE,
+      isClash: TILE_IS_CLASH,
+      isMiniClash: TILE_IS_MINICLASH,
+      curZoneId: CUR_ZONE_ID,
+      currZoneTitle: CURRENT_ZONE_NAME,
+      zoneAttacker: CUR_ZONE_ATTACKER,
+      grandBattles: ROUND_GRANDBATTLES,
+      items: CURRENT_ZONE_ITEMS
+    })
+  },
+  props: {
+    title: String
+  },
+  methods: {
+    mousedOver() {
+      this.hovering = true
+    },
+    mousedOut() {
+      this.hovering = false
+    },
+    selectingClick() {
+      this.$store.commit(NEW_SELECTED, this.title)
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*elements*/
+
+.gboverlay {
+  width: 100%;
+  left: -50%;
+  top: 50%;
+  transform: translate(50%, -60%);
+  z-index: 94;
+  position: absolute;
+  pointer-events: none;
+
+  -khtml-user-select: none;
+  -o-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+}
+
+.zone_title {
+  color: rgb(80, 58, 70);
+  font-weight: bolder;
+  z-index: 200;
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 120%;
+  font-size: 0.9vw;
+
+  font-family: "Permanent Marker", cursive;
+
+  background-image: url("../assets/pics/header-bg.png");
+  background-size: 100% 100%;
+
+  pointer-events: none;
+  -khtml-user-select: none;
+  -o-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+}
+
+.selector {
+  height: 8vw;
+  width: auto;
+  z-index: 97;
+  left: 50%;
+  transform: translate(-50%, -75%);
+  top: 0%;
+  position: absolute;
+  pointer-events: none;
+
+  -khtml-user-select: none;
+  -o-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+}
+
+.selector img {
+  height: 8vw;
+  width: auto;
+}
+
+.selector div {
+  position: absolute;
+  top: -2%;
+  left: 50%;
+  transform: translateX(-50%);
+  color: rgb(3 64 11);
+  font-family: "Permanent Marker", cursive;
+  letter-spacing: 0.1em;
+  font-size: 0.8em;
+}
+
+.mapTile {
+  color: black;
+  padding: 0;
+  text-align: center;
+  position: relative;
+  height: 80%;
+  transition: transform 400ms;
+  overflow: visible;
+}
+
+.battleIndicator {
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  left: 50%;
+  transform: translate(-40%, -50%);
+  z-index: 98;
+  top: 0%;
+  position: absolute;
+
+  -khtml-user-select: none;
+  -o-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+  pointer-events: none;
+}
+
+.terrainGroup {
+  display: inline-block;
+  transition: all 0.3s ease-in;
+  position: absolute;
+  left: 0%;
+  top: 0%;
+  width: 100%;
+  height: 100%;
+}
+
+.terrainTile {
+  transform: translateY(-40%);
+  width: 100%;
+  pointer-events: none;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 90;
+  user-select: none;
+}
+
+.label {
+  position: absolute;
+  top: -80%;
+  left: 50%;
+  transform: translate(-50%, -20%);
+  font-size: 0.9em;
+  width: 70%;
+  z-index: 99;
+
+  background-color: whitesmoke;
+  border: solid black;
+  border-width: 0.02em;
+  padding: 0.2em 0.65em 0.2em 0.65em;
+  border-radius: 0.8em;
+
+  -khtml-user-select: none;
+  -o-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+}
+
+.fieldItem {
+  -khtml-user-select: none;
+  -o-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+
+  position: absolute;
+  left: 50%;
+  top: -70%;
+  transform: translate(-50%, -20%);
+  width: 90%;
+  height: auto;
+
+  z-index: 140;
+}
+
+@media only screen and (max-width: 840px) {
+  .battleIndicator {
+    width: 70%;
+  }
+  .selector {
+    height: 6vh;
+  }
+  .selector div {
+    font-size: 0.35em;
+  }
+}
+
+@media only screen and (orientation: landscape) and (max-width: 840px) {
+  .battleIndicator {
+    width: 60%;
+  }
+  .selector {
+    height: 14vh;
+  }
+}
+
+/*modifiers*/
+
+img {
+  -khtml-user-select: none;
+  -o-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+}
+
+.bastionOwned {
+  filter: hue-rotate(0deg);
+}
+
+.pyreOwned {
+  filter: hue-rotate(129deg);
+}
+
+.unOwned {
+  filter: grayscale();
+}
+
+.simple.unOwned {
+  filter: grayscale() brightness(1.8);
+}
+
+/*animations*/
+
+div.terrainGroupHovered {
+  transform: translateY(-50%);
+}
+
+.stab-enter-active,
+.stab-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.stab-enter /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translate(-50%, -300%);
+  /* top: 500%; */
+}
+.stab-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
+
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translate(-50%, -200%);
+  top: -200%;
+  background-color: purple;
+  color: white;
+}
+
+.fall-enter-active,
+.fall-leave-active {
+  transition: all var(--fall-time) ease-out;
+  position: absolute;
+}
+
+.fall-enter /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translatey(-20vh);
+  /* top: 500%; */
+}
+.fall-leave-to {
+  opacity: 0;
+  transform: translatey(20vh);
+}
+
+.ghost-enter-active,
+.ghost-leave-active {
+  transition: opacity 0.3s ease-out, width 0.9s ease-in-out;
+}
+
+.ghost-enter {
+  opacity: 0;
+  width: 0;
+}
+
+.ghost-leave-to {
+  opacity: 0;
+  width: 0;
+}
+</style>
